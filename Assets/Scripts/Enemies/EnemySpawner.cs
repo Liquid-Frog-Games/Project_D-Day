e0bbc946d866fc5e7e9bd46a242eed5b1218b192
Rob de Willigen <59250958+robdewilligen@users.noreply.github.com>
2023-09-22 15:43:46 +0200

    Revert "60 difficulty scaling"

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -18,6 +18,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int baseEnemies;
     [SerializeField] private float enemiesPerSecond;
     [SerializeField] private float timeBetweenWaves;
+    [SerializeField] private float difficultyScalingFactor;
     [SerializeField] private TextMeshProUGUI roundUI;
 
     public int currentWave = 1;
@@ -37,8 +38,7 @@ public class EnemySpawner : MonoBehaviour
         baseEnemies = 8;
         enemiesPerSecond = 0.5f;
         timeBetweenWaves = 5f;
-        StartCoroutine(StartWave());
-
+        difficultyScalingFactor = 0.75f;
         onEnemyDestroy.AddListener(EnemyDestroyed);
     }
 
@@ -114,10 +114,10 @@ public class EnemySpawner : MonoBehaviour
                 enemySelectMax += 1;
             }
             timeBetweenWaves -= 0.2f;
+            difficultyScalingFactor += 0.25f;
             StartCoroutine(StartWave());
 
         }
-        baseEnemies += 4;
         StartCoroutine(StartWave());
     }
     //on enemy destroyed
@@ -141,7 +141,7 @@ public class EnemySpawner : MonoBehaviour
     //calculates the enemy per wave
     private int EnemiesPerWave()
     {
-        return Mathf.RoundToInt(baseEnemies);
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
     }
 
 

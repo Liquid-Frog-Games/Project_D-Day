5242a9d345f86fef81308c502802ee3ae4ffd9ef
Levi <1006554@hr.nl>
2023-09-22 16:40:47 +0200

    Preview mode
    Preview mode added

--- Assets/Scripts/Handlers/PlotHandler.cs
+++ Assets/Scripts/Handlers/PlotHandler.cs
@@ -1,42 +1,58 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlotHandler : MonoBehaviour
 {
-    public SpriteRenderer sprite;
+    //sprites 
+    public SpriteRenderer spriteRenderer;
+    public Sprite addSprite;
+    public Sprite removeSprite;
 
+    //gameObjects
     public GameObject[] icons;
     private GameObject tower;
+    private Tower towerToBuild;
 
-
+    //canvas
     public Canvas shopCanvas;
     public Canvas previewCanvas;
 
-    public bool isSelected;
+    //booleans 
+    public bool shopOpen;
+    public bool inPreview;
+
 
     private void Start()
     {
-        isSelected = false;
+        spriteRenderer.sprite = addSprite;
+        inPreview = false;
+        shopOpen = false;
         ShopCanvasGroupOff();
         PreviewCanvasGroupOff();
 
     }
     //ui for the shop 
-   
+
     private void ShopCanvasGroupOff()
-    { 
+    {
         //turns the shop UI off
+        shopOpen = false;
+        spriteRenderer.sprite = addSprite;
         shopCanvas.GetComponent<CanvasGroup>().alpha = 0;
         shopCanvas.GetComponent<CanvasGroup>().interactable = false;
         shopCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
-    
+
     private void ShopCanvasGroupOn()
     { //turns the UI on
+        shopOpen = true;
+        spriteRenderer.sprite = removeSprite;
         shopCanvas.GetComponent<CanvasGroup>().alpha = 1;
         shopCanvas.GetComponent<CanvasGroup>().interactable = true;
         shopCanvas.GetComponent<CanvasGroup>().blocksRaycasts = true;
@@ -45,119 +61,92 @@ public class PlotHandler : MonoBehaviour
 
     private void PreviewCanvasGroupOff()
     {
+        //turns the preview UI off
+        inPreview = true;
         previewCanvas.GetComponent<CanvasGroup>().alpha = 0;
         previewCanvas.GetComponent<CanvasGroup>().interactable = false;
         previewCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
-    
+
     private void PreviewCanvasGroupOn()
     {
-        //turns the UI on
+        //turns the preview UI on
+        inPreview = false;
         previewCanvas.GetComponent<CanvasGroup>().alpha = 1;
         previewCanvas.GetComponent<CanvasGroup>().interactable = true;
         previewCanvas.GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 
     //when the player clicks on the plot
+
     public void OnMouseDown()
     {
-        if(isSelected == false)
+
+        if (tower != null || towerToBuild != null) return;
+        if (shopOpen == false)
         {
-            Debug.Log("Work please");
-            isSelected = true;
             ShopCanvasGroupOn();
             return;
 
         }
-        if(isSelected == true)
+        if (shopOpen == true)
         {
-            Debug.Log("Turned off");
-            isSelected= false;
             ShopCanvasGroupOff();
             return;
         }
     }
-    private bool IsPointerOverUIObject()
-        {
-            PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
-            eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            List<RaycastResult> results = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
-            return results.Count > 0;
-        }
 
+    //does the element get covered by UI
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
+    //preview function
     public void PreviewTower()
     {
-        
+        ShopCanvasGroupOff();
+        PreviewCanvasGroupOn();
+
+        towerToBuild = BuildManager.main.GetSelectedTower();
+        if (towerToBuild.cost > LevelManager.main.coins)       //replace with UI message
+
         {
-            
-            
-            PreviewCanvasGroupOn();
-        }
-        return;
-    }
-    public void PlaceTower()
-    {
-        if (IsPointerOverUIObject())
-        {
-            Tower towerToBuild = BuildManager.main.GetSelectedTower();
-            if (towerToBuild.cost > LevelManager.main.coins)
-            {
-                Debug.Log("You dont have enough coins for this towwer");
-                return;
-            }
-            tower = Instantiate(towerToBuild.prefab, transform.position, Quaternion.identity);
-            ShopCanvasGroupOff();
-            LevelManager.main.SpendCurrency(towerToBuild.cost);
-            BuildManager.main.SetSelectedTower(-1);
-            ShopCanvasGroupOff();
+            Debug.Log("You dont have enough coins for this towwer");
+            CancelTower();
             return;
         }
-        return;
-    }
-    /*
-    [Header("References")]
-    [SerializeField] private SpriteRenderer sr;
-    [SerializeField] private Color hoverColor;
 
-    
-    private Color startColor;
-
-    void Start()
-    {
-        startColor = sr.color;
-    }
-
-    
-    private void OnMouseEnter()
-    {
-        sr.color = hoverColor;
-    }
-
-    private void OnMouseExit()
-    {
-        sr.color = startColor;
-    }
-
-    private void OnMouseDown()
-    {
-                   //TODO: Replace return with upgrade options
-        if (tower != null) return;
-  
-        if(IsPointerOverUIObject() == false)
-        {
-        Tower towerToBuild = BuildManager.main.GetSelectedTower();
-            if (towerToBuild.cost > LevelManager.main.coins)
-            {
-                LevelManager.main.StartNotification();
-                return;
-            }
-        LevelManager.main.SpendCurrency(towerToBuild.cost);
         tower = Instantiate(towerToBuild.prefab, transform.position, Quaternion.identity);
-        BuildManager.main.SetSelectedTower(-1);
-        }
+
         return;
     }
-    */
+
+    //placing the tower
+    public void PlaceTower()
+    {
+        tower.GetComponent<TurretHandler>().ToggleActive();
+        LevelManager.main.SpendCurrency(towerToBuild.cost);
+        BuildManager.main.SetSelectedTower(-1);
+        PreviewCanvasGroupOff();
+        spriteRenderer.sprite = null;
+        return;
+    }
+
+    //canceling the preview
+    public void CancelTower()
+    {
+        towerToBuild = null;
+        Destroy(tower);
+        BuildManager.main.SetSelectedTower(-1);
+        PreviewCanvasGroupOff();
+        ShopCanvasGroupOn();
+        return;
+    }
+
 }

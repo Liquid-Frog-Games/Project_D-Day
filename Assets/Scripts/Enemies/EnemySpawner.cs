9a437785fe4ee7b5e7232c9ec2a2ed640c93a634
Levi <1006554@hr.nl>
2023-09-22 14:11:01 +0200

    Update EnemySpawner.cs
    Every wave the baseEnemies goes up by 4.

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -15,7 +15,6 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int baseEnemies;
     [SerializeField] private float enemiesPerSecond;
     [SerializeField] private float timeBetweenWaves;
-    [SerializeField] private float difficultyScalingFactor;
     [SerializeField] private TextMeshProUGUI roundUI;
 
     public int currentWave = 1;
@@ -35,7 +34,6 @@ public class EnemySpawner : MonoBehaviour
         baseEnemies = 8;
         enemiesPerSecond = 0.5f;
         timeBetweenWaves = 5f;
-        difficultyScalingFactor = 0.75f;
         StartCoroutine(StartWave());
         onEnemyDestroy.AddListener(EnemyDestroyed);
     }
@@ -87,10 +85,10 @@ public class EnemySpawner : MonoBehaviour
                 enemySelectMax += 1;
             }
             timeBetweenWaves -= 0.2f;
-            difficultyScalingFactor += 0.25f;
             StartCoroutine(StartWave());
 
         }
+        baseEnemies += 4;
         StartCoroutine(StartWave());
     }
     //on enemy destroyed
@@ -114,7 +112,7 @@ public class EnemySpawner : MonoBehaviour
     //calculates the enemy per wave
     private int EnemiesPerWave()
     {
-        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+        return Mathf.RoundToInt(baseEnemies);
     }
 
 
